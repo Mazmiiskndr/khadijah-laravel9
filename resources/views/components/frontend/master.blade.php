b2063a22f3ced36744d5a319075ca60c8c120ba9
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-04-06 07:49:27 +0700

    :sparkles: (feat): added local fontawesome 6.3

--- resources/views/components/frontend/master.blade.php
+++ resources/views/components/frontend/master.blade.php
@@ -22,7 +22,7 @@
     <x-frontend.css />
     @stack('styles')
     {{-- End Css --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome/css/all.min.css') }}" />
 
 
 
